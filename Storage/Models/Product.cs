a4abcd29498f80c5ddd99b3a8fb2d1041c7ebe89
Radha Manickam <radhaarul@gmail.com>
2022-01-25 14:58:47 +0100

    Add project files.

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+       public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Orderdate { get; set; }
+        [Required]
+        public string Category { get; set; }
+        public string Shelf { get; set; }
+        public int Count { get; set; }
+        public string Description { get; set; }
+    }
+}
